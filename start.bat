33f81618cf09843e1ecd200752ea3a576d9f7003
grupodvbr <grupodvbr@gmail.com>
2026-06-20 12:23:41 -0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist logs (
+  mkdir logs
+)
+
+echo [%date% %time%] Iniciando OTTO Device Agent >> logs\startup.log
+
+node otto-device-agent.js >> logs\console.log 2>&1
